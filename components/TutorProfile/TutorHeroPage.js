a770bfd64f069625fe97812d36a3ce7b0a55715e
Mohit Chahal <work.mohitchahal@gmail.com>
2022-04-17 08:32:40 +0530

    :wrench:

--- components/TutorProfile/TutorHeroPage.js
+++ components/TutorProfile/TutorHeroPage.js
@@ -1,20 +1,23 @@
-import Image from 'next/image';
-import ProfilePhotoPopUp from "./PopUps/ProfilePhotoPopUp";
-import { openPopUps } from "../../Atoms/PopUpAtoms";
-import { useRecoilState } from "recoil";
-import CoverPhotoPopUp from './PopUps/CoverPhotoPopUp';
+import Image from 'next/image'
+import ProfilePhotoPopUp from './PopUps/ProfilePhotoPopUp'
+import { openPopUps } from '../../Atoms/PopUpAtoms'
+import { useRecoilState } from 'recoil'
+import CoverPhotoPopUp from './PopUps/CoverPhotoPopUp'
 
 export default function TutorHeroPage() {
   // top open and close popup
-  const [profilePopUp, setProfilePopUp] = useRecoilState(openPopUps);
-  const [CoverPopUp, setCoverPopUp] = useRecoilState(openPopUps);
+  const [profilePopUp, setProfilePopUp] = useRecoilState(openPopUps)
+  const [CoverPopUp, setCoverPopUp] = useRecoilState(openPopUps)
 
   return (
-    <div className="snap-fullPage relative hidden md:flex h-[calc(100vh-79px)] items-center justify-start overflow-hidden bg-gradient-to-r from-[#FF7C7CB8] to-[#C4C4C400]">
+    <div className="snap-fullPage relative hidden h-[calc(100vh-79px)] items-center justify-start overflow-hidden bg-gradient-to-r from-[#FF7C7CB8] to-[#C4C4C400] md:flex">
       <BackgroundImg />
       {/* camera  */}
-      <div onClick={() => setCoverPopUp({ ...false, CoverPhotoPopUp: true })} className='absolute z-[1] top-[15px] right-[15px] cursor-pointer'>
-        <div className='relative w-[27.5px] h-[22.5px]'>
+      <div
+        onClick={() => setCoverPopUp({ ...false, CoverPhotoPopUp: true })}
+        className="absolute top-[15px] right-[15px] z-[1] cursor-pointer"
+      >
+        <div className="relative h-[22.5px] w-[27.5px]">
           <Image
             src="/Images/TutorProfile/camera.png"
             alt="background"
@@ -25,49 +28,30 @@ export default function TutorHeroPage() {
       </div>
 
       {/* Profession */}
-      <p className='hidden lg:block absolute w-full text-center -bottom-6 font-poppins font-semibold text-[100px] text-[#424242] uppercase opacity-50 tracking-[25px]'>
-        <span className='text-[#FFFFFF]'>
-          Professional
-        </span> chef
+      <p className="absolute -bottom-6 hidden w-full text-center font-poppins text-[100px] font-semibold uppercase tracking-[25px] text-[#424242] opacity-50 lg:block">
+        <span className="text-[#FFFFFF]">Professional</span> chef
       </p>
 
       {/* ProfileChip */}
       <ProfileChip />
 
-<<<<<<< Updated upstream
       {/* main */}
-      <div className='flex items-center gap-8 lg:pl-16 pl-8'>
+      <div className="flex items-center gap-8 pl-8 lg:pl-16">
         <UserImage setProfilePopUp={setProfilePopUp} />
 
         {/* profile photo popup */}
-        {profilePopUp.ProfilePhotoPopUp && (
-          <ProfilePhotoPopUp />
-        )}
+        {profilePopUp.ProfilePhotoPopUp && <ProfilePhotoPopUp />}
 
-        {CoverPopUp.CoverPhotoPopUp && (
-          <CoverPhotoPopUp />
-        )}
+        {CoverPopUp.CoverPhotoPopUp && <CoverPhotoPopUp />}
       </div>
-=======
-function BackgroundFullImg({ user_data }) {
-  return (
-    <div className="absolute top-0 -z-30  h-full  w-full ">
-      <Image
-        priority
-        src={`https://akbh.s3.ap-south-1.amazonaws.com/skillshare/user/profile_img/${user_data.background_pic}`}
-        layout="fill"
-        objectFit="cover"
-        alt="yo"
-      />
->>>>>>> Stashed changes
     </div>
   )
 }
 
 const BackgroundImg = () => {
   return (
-    <div className='absolute h-full w-full'>
-      <div className='relative h-full w-full -z-10'>
+    <div className="absolute h-full w-full">
+      <div className="relative -z-10 h-full w-full">
         <Image
           src="/Images/TutorProfile/background.png"
           alt="background"
@@ -81,10 +65,13 @@ const BackgroundImg = () => {
 
 const UserImage = ({ setProfilePopUp }) => {
   return (
-    <div className='relative lg:w-[280px] lg:h-[280px] w-[200px] h-[200px] rounded-lg'>
+    <div className="relative h-[200px] w-[200px] rounded-lg lg:h-[280px] lg:w-[280px]">
       {/* camera */}
-      <div onClick={() => setProfilePopUp({ ...false, ProfilePhotoPopUp: true })} className='absolute z-[1] top-[15px] right-[15px] cursor-pointer'>
-        <div className='relative w-[27.5px] h-[22.5px]'>
+      <div
+        onClick={() => setProfilePopUp({ ...false, ProfilePhotoPopUp: true })}
+        className="absolute top-[15px] right-[15px] z-[1] cursor-pointer"
+      >
+        <div className="relative h-[22.5px] w-[27.5px]">
           <Image
             src="/Images/TutorProfile/camera.png"
             alt="background"
@@ -96,17 +83,10 @@ const UserImage = ({ setProfilePopUp }) => {
 
       {/* userImage */}
       <Image
-<<<<<<< Updated upstream
-        src='/Images/TutorProfile/tutor.png'
-        alt='tutor'
-        layout='fill'
-        objectFit='contain'
-=======
-        src={`https://akbh.s3.ap-south-1.amazonaws.com/skillshare/user/profile_img/${user_data.profile_img}`}
-        height={327}
-        width={224}
-        alt="yo"
->>>>>>> Stashed changes
+        src="/Images/TutorProfile/tutor.png"
+        alt="tutor"
+        layout="fill"
+        objectFit="contain"
       />
     </div>
   )
@@ -114,121 +94,28 @@ const UserImage = ({ setProfilePopUp }) => {
 
 const ProfileChip = () => {
   return (
-    <div className='flex items-center justify-between absolute lg:bottom-[100px] bottom-[60px] lg:left-[60px] left-[30px] sm:w-[300px] w-[216px] h-[40px] bg-[#FFFFFF] rounded-full px-4 py-2 font-roboto shadow-xl'>
+    <div className="absolute bottom-[60px] left-[30px] flex h-[40px] w-[216px] items-center justify-between rounded-full bg-[#FFFFFF] px-4 py-2 font-roboto shadow-xl sm:w-[300px] lg:bottom-[100px] lg:left-[60px]">
       <div>
-<<<<<<< Updated upstream
-        <svg width="5" height="20" viewBox="0 0 5 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg
+          width="5"
+          height="20"
+          viewBox="0 0 5 29"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <circle cx="2.5" cy="2.5" r="2.5" fill="#FC4D6D" />
           <circle cx="2.5" cy="14.5" r="2.5" fill="#FC4D6D" />
           <circle cx="2.5" cy="26.5" r="2.5" fill="#FC4D6D" />
         </svg>
-=======
-        <div className="font-bold">
-          <div className=" mt-1 text-[24px] tracking-[0.175em]">
-            {user_data.headline}
-          </div>
-          <h1 className=" flex gap-[17px] text-[56px]">
-            <span> {user_data.name}</span>
-            <div className="inline-block h-[21px] w-[21px]">
-              {user_data.active ? (
-                <Image
-                  src={'/Images/TutorProfile/svg/right-ok.svg'}
-                  height={21}
-                  width={21}
-                  alt="yo"
-                />
-              ) : null}
-            </div>
-            {/* <div className="inline-block">
-              <Image
-                src={'/Images/TutorProfile/svg/australia.svg'}
-                height={19}
-                width={19}
-              />
-            </div> */}
-          </h1>
-        </div>
-        <p className="mb-[18px] font-[600] tracking-[.065em]">
-          {user_data.teaching_experience}
-        </p>
-        <div className="text-[18px] font-[500]">
-          <TutorDescription user_data={user_data} />
-          <div className="mt-[28px] flex items-center gap-[27px]">
-            <Link href={'/'}>
-              <a className="btn-shadow rounded-full bg-[#FC4D6D] px-[19px]   py-[9px] font-bold tracking-[0.01em] transition delay-75 ease-in-out hover:bg-[#e02e4ff5]">
-                Book Trial
-              </a>
-            </Link>
-            <div className="text-[20px] normal-case ">
-              {' '}
-              Rs.{user_data.hourly_rate}/hr
-            </div>
-          </div>
-        </div>
->>>>>>> Stashed changes
       </div>
 
-<<<<<<< Updated upstream
-      <button className='text-[#FC4D6D] font-extrabold text-[16px]'>
+      <button className="text-[16px] font-extrabold text-[#FC4D6D]">
         Edit Profile
       </button>
 
-      <button className='text-[#828282] font-extrabold text-[16px]'>
+      <button className="text-[16px] font-extrabold text-[#828282]">
         Upload Profile
       </button>
-=======
-function TutorDescription({ user_data }) {
-  return (
-    <div className="ml-[7px] font-[500] tracking-[0.065em]  ">
-      <div className="flex gap-[10px]  ">
-        <div className="ml-[-7px] flex justify-center">
-          <Image
-            src={'/Images/TutorProfile/svg/topper-cap.svg'}
-            height={20}
-            width={22}
-            alt="yo"
-          />
-        </div>
-        <span>
-          Teaches{' '}
-          {user_data?.subject_taught_id
-            ?.map(function (elem) {
-              return SUBJECTS[elem]
-            })
-            .join(', ')}{' '}
-          language
-        </span>
-      </div>
-      <div className="flex items-center gap-[10px]">
-        <div className="  ml-[-4px]  mr-[5px] flex justify-center">
-          {/* <Image
-            src={'/Images/TutorProfile/svg/message.svg'}
-            height={13.5}
-            width={13.5}
-          /> */}
-          <FiMessageCircle />
-        </div>
-        <span>
-          Speaks {/* FIX */}
-          {/* {user_data.language_spoken
-            .map(function (elem) {
-              return elem.language
-            })
-            .join(', ')} */}
-        </span>
-      </div>
-      <div className="flex gap-[10px]  ">
-        <div className="ml-[-2px] mr-[4px] flex justify-center">
-          <Image
-            src={'/Images/TutorProfile/svg/circles.svg'}
-            height={16}
-            width={16}
-            alt="yo"
-          />
-        </div>
-        <span>0 lessons</span>
-      </div>
->>>>>>> Stashed changes
     </div>
   )
 }
--- components/TutorProfile/TutorHeroPageMobile.js
+++ components/TutorProfile/TutorHeroPageMobile.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import Image from 'next/image';
-import WhiteTick from "../../icons/WhiteTick";
-import HatIcon from "../../icons/HatIcon";
-import CommentIcon from "../../icons/CommentIcon";
-import CirclesIcon from "../../icons/CirclesIcon";
-import { StarIcon } from "../../icons/FlagIcon";
+import React from 'react'
+import Image from 'next/image'
+import WhiteTick from '../../icons/WhiteTick'
+import HatIcon from '../../icons/HatIcon'
+import CommentIcon from '../../icons/CommentIcon'
+import CirclesIcon from '../../icons/CirclesIcon'
+import { StarIcon } from '../../icons/FlagIcon'
 
 function TutorHeroPageMobile() {
   return (
-    <div className="snap-fullPage h-[calc(100vh-79px)] relative flex md:hidden items-start justify-start capitalize">
+    <div className="snap-fullPage relative flex h-[calc(100vh-79px)] items-start justify-start capitalize md:hidden">
       <BackgroundImgContainer />
 
-      <div className='flex items-center justify-start flex-col w-full h-full gap-12 sm:gap-14'>
+      <div className="flex h-full w-full flex-col items-center justify-start gap-12 sm:gap-14">
         <NameText />
 
         {/* photo and other things container */}
-        <div className='flex items-center justify-between sm:px-10 px-6 w-full whitespace-normal gap-6'>
+        <div className="flex w-full items-center justify-between gap-6 whitespace-normal px-6 sm:px-10">
           <ProfileImg />
           <Details />
         </div>
@@ -27,15 +27,13 @@ function TutorHeroPageMobile() {
   )
 }
 
-export default TutorHeroPageMobile;
-
+export default TutorHeroPageMobile
 
 // BackgroundImgContainer
 const BackgroundImgContainer = () => {
   return (
-<<<<<<< Updated upstream
-    <div className='absolute h-[60%] w-full bg-gradient-to-r from-[#FF7C7CB8] to-[rgba(255,189,109,0.72)]'>
-      <div className='relative h-full w-full -z-10'>
+    <div className="absolute h-[60%] w-full bg-gradient-to-r from-[#FF7C7CB8] to-[rgba(255,189,109,0.72)]">
+      <div className="relative -z-10 h-full w-full">
         <Image
           src="/Images/TutorProfile/background.png"
           alt="background"
@@ -43,195 +41,94 @@ const BackgroundImgContainer = () => {
           layout="fill"
         />
       </div>
-=======
-    <div className="absolute top-0 -z-30  h-full  w-full ">
-      <Image
-        priority
-        src={`https://akbh.s3.ap-south-1.amazonaws.com/skillshare/user/profile_img/${user_data.background_pic}`}
-        layout="fill"
-        objectFit="cover"
-        alt="yo"
-      />
->>>>>>> Stashed changes
     </div>
   )
 }
 
-
-// top text 
+// top text
 const NameText = () => {
   return (
-<<<<<<< Updated upstream
-    <div className='flex items-center justify-center w-full flex-col gap-4 z-10 font-poppins font-bold text-[#FFFFFF] py-20 sm:py-16'>
-
-      <p className='tracking-[0.15em] text-[24px]'>
-        Hello I&apos;m
-      </p>
-      <div className='flex items-center justify-center gap-2 tracking-widest sm:text-[40px] text-[35px]'>
-        <h2>
-          Andrew Smith
-        </h2>
-        <div className='flex items-center justify-center gap-2'>
+    <div className="z-10 flex w-full flex-col items-center justify-center gap-4 py-20 font-poppins font-bold text-[#FFFFFF] sm:py-16">
+      <p className="text-[24px] tracking-[0.15em]">Hello I&apos;m</p>
+      <div className="flex items-center justify-center gap-2 text-[35px] tracking-widest sm:text-[40px]">
+        <h2>Andrew Smith</h2>
+        <div className="flex items-center justify-center gap-2">
           <WhiteTick />
           {/* this is a flag */}
           <StarIcon />
-=======
-    <div className="-mt-20 space-y-4 text-center font-bold capitalize text-white ">
-      <div className=" -ml-8 text-sm tracking-[0.175em]">
-        {user_data.headline}
-      </div>
-      <h1 className=" flex gap-[17px] text-3xl">
-        <span> {user_data.name}</span>
-        <div className="inline-block h-[21px] w-[21px]">
-          <Image
-            src={'/Images/TutorProfile/svg/right-ok.svg'}
-            height={21}
-            width={21}
-            alt="yo"
-          />
-        </div>
-        <div className="inline-block">
-          <Image
-            src={'/Images/TutorProfile/svg/australia.svg'}
-            height={19}
-            width={19}
-            alt="yo"
-          />
->>>>>>> Stashed changes
         </div>
       </div>
     </div>
   )
 }
-
 
 // Tutor Img
 function ProfileImg() {
   return (
     // comment out this div when the image is real without gradient borders
     // <div className='bg-gradient-to-br from-[#FC4D6D] to-[#FDA02F] p-[2px] rounded-xl'>
-    <div className="relative sm:h-[180px] sm:w-[155px] h-[150px] w-[131px] overflow-hidden rounded-xl">
+    <div className="relative h-[150px] w-[131px] overflow-hidden rounded-xl sm:h-[180px] sm:w-[155px]">
       <Image
-        src='/Images/TutorProfile/tutor-mobile-img.png'
+        src="/Images/TutorProfile/tutor-mobile-img.png"
         objectFit="contain"
         layout="fill"
-        alt='tutor'
+        alt="tutor"
       />
     </div>
     // </div>
   )
 }
 
-
 // Details
 const Details = () => {
   return (
-<<<<<<< Updated upstream
-    <div className='flex flex-col justify-between gap-4 z-10 flex-1 h-full tracking-wider'>
-      <div className='flex flex-col font-poppins font-bold text-[#FFFFFF] text-[20px]'>
-        <p>
-          Qualified TEFL Tutor and
-        </p>
-        <p>
-          Accent Coach With
-        </p>
-=======
-    <div className="ml-[7px] space-y-1  text-xs font-[500] tracking-[0.065em] text-[#616161]  ">
-      <div className="flex gap-[10px]  ">
-        <div className="ml-[-7px] flex justify-center">
-          <Image
-            src={'/Images/TutorProfile/svg/topper-cap-gray.svg'}
-            priority
-            height={16}
-            width={16}
-            alt="yo"
-          />
-        </div>
-        <span>
-          Teaches {/* FIX */}
-          {/* {user_data?.subject_taught_id
-            ?.map(function (elem) {
-              return SUBJECTS[elem]
-            })
-            .join(', ')}{' '} */}
-          language
-        </span>
->>>>>>> Stashed changes
+    <div className="z-10 flex h-full flex-1 flex-col justify-between gap-4 tracking-wider">
+      <div className="flex flex-col font-poppins text-[20px] font-bold text-[#FFFFFF]">
+        <p>Qualified TEFL Tutor and</p>
+        <p>Accent Coach With</p>
       </div>
-      <div className='flex flex-col items-start gap-1 text-[#616161] font-medium font-roboto text-[16px]'>
-        <div className='flex items-center gap-2'>
+      <div className="flex flex-col items-start gap-1 font-roboto text-[16px] font-medium text-[#616161]">
+        <div className="flex items-center gap-2">
           <HatIcon />
-          <p>
-            Teaches English Language
-          </p>
+          <p>Teaches English Language</p>
         </div>
-<<<<<<< Updated upstream
-        <div className='flex items-center gap-2'>
+        <div className="flex items-center gap-2">
           <CommentIcon />
-          <p>
-            Speaks English Native Italian B2
-          </p>
+          <p>Speaks English Native Italian B2</p>
         </div>
-        <div className='flex items-center gap-2'>
+        <div className="flex items-center gap-2">
           <CirclesIcon />
-          <p>
-            207 Lessons
-          </p>
-=======
-        <span>
-          Speaks {/* FIX */}
-          {/* {user_data.language_spoken
-            .map(function (elem) {
-              return elem.language
-            })
-            .join(', ')}{' '} */}
-        </span>
-      </div>
-      <div className="flex gap-[10px]  ">
-        <div className="ml-[-2px] mr-[4px] flex justify-center">
-          <Image
-            src={'/Images/TutorProfile/svg/circles-gray.svg'}
-            height={13.33}
-            width={13.33}
-            alt="yo"
-          />
->>>>>>> Stashed changes
+          <p>207 Lessons</p>
         </div>
       </div>
     </div>
   )
 }
 
-
 // chip
 const ProfileChip = () => {
   return (
-<<<<<<< Updated upstream
-    <div className='flex items-center justify-between absolute bottom-[100px] right-[30px] w-[280px] h-[50px] bg-[#FFFFFF] tracking-widest rounded-full px-4 py-2 font-roboto shadow-xl z-30'>
-      <button className='text-[#FC4D6D] font-extrabold text-[16px]'>
+    <div className="absolute bottom-[100px] right-[30px] z-30 flex h-[50px] w-[280px] items-center justify-between rounded-full bg-[#FFFFFF] px-4 py-2 font-roboto tracking-widest shadow-xl">
+      <button className="text-[16px] font-extrabold text-[#FC4D6D]">
         Edit Profile
       </button>
 
-      <button className='text-[#828282] font-extrabold text-[16px]'>
+      <button className="text-[16px] font-extrabold text-[#828282]">
         Upload Profile
       </button>
 
       <div>
-        <svg width="5" height="20" viewBox="0 0 5 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg
+          width="5"
+          height="20"
+          viewBox="0 0 5 29"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <circle cx="2.5" cy="2.5" r="2.5" fill="#FC4D6D" />
           <circle cx="2.5" cy="14.5" r="2.5" fill="#FC4D6D" />
           <circle cx="2.5" cy="26.5" r="2.5" fill="#FC4D6D" />
         </svg>
-=======
-    <div>
-      <div className="relative h-36 w-32 overflow-hidden rounded-lg bg-gradient-to-r from-[#FC4D6D] to-[#FDA02F] p-2">
-        <Image
-          src={`https://akbh.s3.ap-south-1.amazonaws.com/skillshare/user/profile_img/${profile_img}`}
-          objectFit="contain"
-          layout="fill"
-          alt="yo"
-        />
->>>>>>> Stashed changes
       </div>
     </div>
   )
