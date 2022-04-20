5bb29d43b4e41391c42e124798249a40e01322e0
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-04-20 19:53:54 +0530

    updated cover photo popup

--- components/TutorProfile/PopUps/CoverPhotoPopUp.js
+++ components/TutorProfile/PopUps/CoverPhotoPopUp.js
@@ -1,80 +1,94 @@
-import React from 'react'
-import CrossIcon from '../../../icons/CrossIcon'
-import WhatsAppTree from '../../../icons/WhatsAppTree.jsx'
-import { openPopUps } from '../../../Atoms/PopUpAtoms'
-import { useRecoilState } from 'recoil'
+import React, { useState, useEffect } from 'react';
+import CrossIcon from "../../../icons/CrossIcon";
+import WhatsAppTree from "../../../icons/WhatsAppTree.jsx";
+import { openPopUps } from "../../../Atoms/PopUpAtoms";
+import { useRecoilState } from "recoil";
+import Dropzone, { useDropzone } from 'react-dropzone'
 
 const CoverPhotoPopUp = () => {
-  const [openPopUp, setOpenPopUp] = useRecoilState(openPopUps)
+    const [openPopUp, setOpenPopUp] = useRecoilState(openPopUps);
 
-  return (
-    <div className="z-30 flex h-[400px] w-[500px] flex-col items-center gap-9 rounded-xl bg-[#FFFFFF] p-6 font-roboto shadow-2xl lg:h-[522px] lg:w-[590px]">
-      {/* Title and CrossButton */}
-      <TopTitle setOpenPopUp={setOpenPopUp} />
+    const [files, setFiles] = useState([]);
 
-      {/* Upload Box */}
-      <UploadBox />
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: 'image/*',
+        onDrop: (acceptedFiles) => {
+            setFiles(
+                acceptedFiles.map((file) =>
+                    Object.assign(file, {
+                        preview: URL.createObjectURL(file),
+                    })
+                )
+            )
+        },
+    })
 
-      {/* Bottom Buttons */}
-      <div className="flex w-full items-center justify-between px-2">
-        <Buttons
-          label="Upload Photo"
-          onClick={() => {}}
-          styles="border-[1.2px] border-[#FC4D6D] text-[#545454] font-bold"
-        />
-        {/* <Buttons
-          label="Edit Cover Title"
-          styles="border-[1.2px] border-[#FC4D6D] text-[#545454] font-bold"
-        /> */}
-        <Buttons
-          label="Save Changes"
-          styles="font-medium text-[#FFFFFF] bg-[#FC4D6D]"
-        />
-      </div>
-    </div>
-  )
+    useEffect(() => {
+        console.log(files);
+    }, [files])
+
+    return (
+        <div className='flex flex-col p-6 items-center gap-9 bg-[#FFFFFF] lg:w-[590px] lg:h-[522px] w-[500px] h-[400px] font-roboto rounded-xl shadow-2xl z-30'>
+            {/* Title and CrossButton */}
+            <TopTitle setOpenPopUp={setOpenPopUp} />
+
+            {/* Upload Box */}
+            <div {...getRootProps({ className: 'dropzone lg:w-[523.32px] lg:h-[311.8px] w-[450px] h-[300px]' })} className='flex items-center justify-center lg:w-[523.32px] lg:h-[311.8px] w-[450px] h-[300px] relative border-[0.33px] border-[#7E7E7E] border-dotted'>
+                <input {...getInputProps()} />
+                <WhatsAppTree styles='absolute' />
+                <div className='flex items-center gap-3 flex-col text-center font-bold'>
+                    <p className='text-[#838383] text-[18px]'>
+                        Drag And Drop Your Photo Here
+                    </p>
+                    <div className='flex items-center flex-col text-[#545454] text-[12px]'>
+                        <p>
+                            JPG,PNG Or WEBP Format
+                        </p>
+                        <p>
+                            Maximum Size - 2MB.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            {/* Bottom Buttons */}
+            <div className='flex items-center justify-between w-full px-2'>
+                {/* upload photo button */}
+                <button {...getRootProps({
+                    className: 'lg:w-[156.29px] lg:h-[42.59px] w-[120px] h-[38px] text-center rounded-lg border-[1.2px] border-[#FC4D6D] text-[#545454] font-bold',
+                })}>
+                    <input {...getInputProps()} />
+                    <p>Upload Photo</p>
+                </button>
+
+                <Buttons label='Edit Cover Title' styles='border-[1.2px] border-[#FC4D6D] text-[#545454] font-bold' />
+                <Buttons label='Save Changes' styles='font-medium text-[#FFFFFF] bg-[#FC4D6D]' />
+            </div>
+        </div>
+    )
 }
 
-export default CoverPhotoPopUp
+export default CoverPhotoPopUp;
+
 
 // Top Title
 const TopTitle = ({ setOpenPopUp }) => {
-  return (
-    <div className="flex w-full items-center justify-between pl-2">
-      <p className="text-xl font-bold text-[#3F3F3F]">Upload Cover Photo</p>
-      <span onClick={() => setOpenPopUp({ ...false, walletPopUp: false })}>
-        <CrossIcon />
-      </span>
-    </div>
-  )
+    return (
+        <div className='flex w-full justify-between items-center pl-2'>
+            <p className='font-bold text-xl text-[#3F3F3F]'>Upload Cover Photo</p>
+            <span onClick={() => setOpenPopUp({ ...false })}>
+                <CrossIcon />
+            </span>
+        </div>
+    )
 }
 
-// Upload Box
-const UploadBox = () => {
-  return (
-    <div className="relative flex h-[300px] w-[450px] items-center justify-center border-[0.33px] border-dotted border-[#7E7E7E] lg:h-[311.8px] lg:w-[523.32px]">
-      <WhatsAppTree styles="absolute" />
-      <div className="flex flex-col items-center gap-3 text-center font-bold">
-        <p className="text-[18px] text-[#838383]">
-          Drag And Drop Your Photo Here
-        </p>
-        <div className="flex flex-col items-center text-[12px] text-[#545454]">
-          <p>JPG,PNG Or WEBP Format</p>
-          <p>Maximum Size - 2MB.</p>
-        </div>
-      </div>
-    </div>
-  )
-}
 
 // Buttons
-const Buttons = ({ label, styles, onClk }) => {
-  return (
-    <button
-      onClick={onClk}
-      className={`h-[38px] w-[120px] rounded-lg text-center lg:h-[42.59px] lg:w-[156.29px] ${styles}`}
-    >
-      {label}
-    </button>
-  )
+const Buttons = ({ label, styles }) => {
+    return (
+        <button className={`lg:w-[156.29px] lg:h-[42.59px] w-[120px] h-[38px] text-center rounded-lg ${styles}`}>
+            {label}
+        </button>
+    )
 }
